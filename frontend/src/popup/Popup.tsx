188c82c002ce29d5d266c84a816fcb515775121e
GurAyDemiroz <guraydemiroz7@gmail.com>
2026-04-05 20:20:15 +0300

    feat: manifest v3 chrome eklentisi altyapısı kuruldu
    Closes #1

---
+++ frontend/src/popup/Popup.tsx
@@ -0,0 +1,9 @@
+export default function Popup() {
+  return (
+   <div className="w-100 h-130 
+   bg-gray-950 text-white 
+   flex items-center justify-center">
+  <p className="text-blue-100 text-xl font-semibold">MeetAgent</p>
+</div>
+  )
+}
